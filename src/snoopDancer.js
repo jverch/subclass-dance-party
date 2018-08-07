bb070d93f3f29c7583b808d59a45a8dd348010f0
jverch <verchjames@gmail.com>
2018-08-07 11:39:59 -0700

    fix oldStep call

--- src/snoopDancer.js
+++ src/snoopDancer.js
@@ -18,7 +18,7 @@ makeSnoopDancer.prototype.oldStep = makeDancer.prototype.step;
 makeSnoopDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
 
-  this.oldStep.call(this);
+  this.oldStep();
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
@@ -26,6 +26,6 @@ makeSnoopDancer.prototype.step = function() {
 };
 
 makeSnoopDancer.prototype.lineUp = function(top, left) {
-  this.setPosition(top - 50, left);
+  this.setPosition(top, left);
 };
 
--- src/steveDancer.js
+++ src/steveDancer.js
@@ -18,7 +18,7 @@ makeSteveDancer.prototype.oldStep = makeDancer.prototype.step;
 makeSteveDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
 
-  this.oldStep.call(this);
+  this.oldStep();
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
